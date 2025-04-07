4c01923dc2a2af0bf9b75a3f04bdecd4ccd05446
Maurits Kienhuis <mauritskienhuis@gmail.com>
2025-04-07 16:02:53 +0200

    Add proper equality support to sequential

--- pumpkin-solver/src/constraints/arithmetic/inequality.rs
+++ pumpkin-solver/src/constraints/arithmetic/inequality.rs
@@ -58,7 +58,7 @@ impl<Var: IntegerVariable + 'static> Constraint for Inequality<Var> {
         } else {
             match solver.satisfaction_solver.internal_parameters.linear_inequality_type {
                 LinearInequalityType::Incremental => {LinearLessOrEqualPropagatorDefault::new(self.terms, self.rhs).post(solver, tag)},
-                LinearInequalityType::Sequential => {LinearLessOrEqualPropagatorSequential::new(self.terms, self.rhs, solver.satisfaction_solver.internal_parameters.linear_ordering.clone(), solver.satisfaction_solver.internal_parameters.linear_group_size).post(solver, tag)}
+                LinearInequalityType::Sequential => {LinearLessOrEqualPropagatorSequential::new(self.terms, self.rhs, solver.satisfaction_solver.internal_parameters.linear_ordering.clone(), solver.satisfaction_solver.internal_parameters.linear_group_size, false).post(solver, tag)}
                 LinearInequalityType::Totalizer => {LinearLessOrEqualPropagatorTotalizer::new(self.terms, self.rhs, solver.satisfaction_solver.internal_parameters.linear_ordering.clone(), solver.satisfaction_solver.internal_parameters.linear_group_size, false).post(solver, tag)},
             }
         }
