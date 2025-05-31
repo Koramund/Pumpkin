01fca06c89411e7b2189ff505a19782f9b52462a
Maurits Kienhuis <mauritskienhuis@gmail.com>
2025-05-31 18:47:10 +0200

    fix: GEQ used incomplete propagation reasons

--- pumpkin-solver/src/engine/constraint_satisfaction_solver.rs
+++ pumpkin-solver/src/engine/constraint_satisfaction_solver.rs
@@ -1272,7 +1272,7 @@ impl ConstraintSatisfactionSolver {
                     }
                 },
             }
-            pumpkin_assert_simple!(
+            pumpkin_assert_extreme!(
                 DebugHelper::debug_check_propagations(
                     num_trail_entries_before,
                     propagator_id,
